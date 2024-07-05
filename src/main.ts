ea692b25ff47f4cbb17dfb742ce535f543232717
xiaotingtingniuniu <424338316@qq.com>
2024-07-05 15:56:35 +0800

    feat: element-ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
-createApp(App).mount('#app')
-const fun = () => {
-  console.log('这是没有格式化的代码')
-}
-fun()
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+const app = createApp(App);
+app.use(ElementPlus);
+app.mount('#app');
