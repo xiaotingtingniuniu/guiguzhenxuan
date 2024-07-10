48cc9b48cdcb8d9d5117324c1f5a074d1369750b
xiaotingtingniuniu <424338316@qq.com>
2024-07-10 19:25:14 +0800

    style: style

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,13 +7,26 @@
           <h1>Hello</h1>
           <h2>欢迎来到硅谷甄选</h2>
           <el-form-item>
-            <el-input :prefix-icon="User" v-model="loginForm.userName"/>
+            <el-input :prefix-icon="User" v-model="loginForm.userName" />
           </el-form-item>
           <el-form-item>
-            <el-input type="password" autocomplete="off" :prefix-icon="Lock" v-model="loginForm.password" show-password/>
+            <el-input
+              type="password"
+              autocomplete="off"
+              :prefix-icon="Lock"
+              v-model="loginForm.password"
+              show-password
+            />
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" size="default" class="login_btn" @click="login">登录</el-button>
+            <el-button
+              type="primary"
+              size="default"
+              class="login_btn"
+              @click="login"
+            >
+              登录
+            </el-button>
           </el-form-item>
         </el-form>
       </el-col>
@@ -22,13 +35,13 @@
 </template>
 
 <script setup lang="ts">
-import { User,Lock } from '@element-plus/icons-vue'
-import { reactive } from 'vue';
+import { User, Lock } from '@element-plus/icons-vue'
+import { reactive } from 'vue'
 //收集账号和密码
-const loginForm = reactive({userName:'admin',password:'111111'});
+const loginForm = reactive({ userName: 'admin', password: '111111' })
 //点击登录按钮
-const login = ()=>{
-  console.log('登录');
+const login = () => {
+  console.log('登录')
 }
 </script>
 
@@ -38,24 +51,24 @@ const login = ()=>{
   height: 100vh;
   background: url('../../assets/images/background.jpg') no-repeat;
   background-size: cover;
-  .login_form{
-    width:80%;
+  .login_form {
+    width: 80%;
     position: relative;
-    top:30vh;
+    top: 30vh;
     background: url('../../assets/images/login_form.png') no-repeat;
     background-size: cover;
-    padding:40px;
-    h1{
+    padding: 40px;
+    h1 {
       font-size: 40px;
-      color:white;
+      color: white;
     }
-    h2{
+    h2 {
       font-size: 20px;
-      color:white;
-      margin:20px 0;
+      color: white;
+      margin: 20px 0;
     }
-    .login_btn{
-      width:100%;
+    .login_btn {
+      width: 100%;
     }
   }
 }
