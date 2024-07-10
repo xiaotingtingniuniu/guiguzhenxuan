5d549541420ad0e3f7d4842d817fff7dfd0e1f5c
xiaotingtingniuniu <424338316@qq.com>
2024-07-10 19:24:29 +0800

    feat: add login page

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,11 @@
 <template>
   <div class="box">
-    <h1>App根组件</h1>
-    <h2>我的</h2>
-    <el-button type="primary" :icon="Edit" />
-    <i class="iconfont icon-settings"></i>
-    <Test></Test>
     <router-view></router-view>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Edit } from '@element-plus/icons-vue'
-import './style/icon.scss'
-import { login, userInfor } from '@/api/user/index.ts'
-import { onMounted } from 'vue'
-const data = {
-  username: 'admin',
-  password: '111111',
-}
-onMounted(() => {
-  //登录接口
-  getLogin()
-})
-//登录
-const getLogin = async () => {
-  const result = await login(data)
-  console.log('result', result)
-  const { token } = result.data
-  console.log('token', token)
-  localStorage.setItem('token', token)
-  getUserInfor()
-}
-//获取用户信息
-const getUserInfor = async () => {
-  const result = await userInfor()
-  console.log('result', result)
-}
 </script>
 
 <style scoped lang="scss">
-.box {
-  color: $color;
-}
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,62 @@
 <template>
-  <div>登录组件</div>
+  <div class="login-container">
+    <el-row>
+      <el-col :span="12" :xs="0"></el-col>
+      <el-col :span="12" :xs="24">
+        <el-form class="login_form">
+          <h1>Hello</h1>
+          <h2>欢迎来到硅谷甄选</h2>
+          <el-form-item>
+            <el-input :prefix-icon="User" v-model="loginForm.userName"/>
+          </el-form-item>
+          <el-form-item>
+            <el-input type="password" autocomplete="off" :prefix-icon="Lock" v-model="loginForm.password" show-password/>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" size="default" class="login_btn" @click="login">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { User,Lock } from '@element-plus/icons-vue'
+import { reactive } from 'vue';
+//收集账号和密码
+const loginForm = reactive({userName:'admin',password:'111111'});
+//点击登录按钮
+const login = ()=>{
+  console.log('登录');
+}
+</script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.login-container {
+  width: 100%;
+  height: 100vh;
+  background: url('../../assets/images/background.jpg') no-repeat;
+  background-size: cover;
+  .login_form{
+    width:80%;
+    position: relative;
+    top:30vh;
+    background: url('../../assets/images/login_form.png') no-repeat;
+    background-size: cover;
+    padding:40px;
+    h1{
+      font-size: 40px;
+      color:white;
+    }
+    h2{
+      font-size: 20px;
+      color:white;
+      margin:20px 0;
+    }
+    .login_btn{
+      width:100%;
+    }
+  }
+}
+</style>
